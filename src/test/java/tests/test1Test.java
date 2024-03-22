704f66a97bde63a7e7b42b18d78c7fbd7708ba2a
reshmaQspi <reshmaQspi@gmail.com>
2024-03-22 08:33:22 +0530

    practice project committed

---
+++ src/test/java/tests/test1Test.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test1Test {
+
+	@Test
+	public static void test1() {
+		System.out.println("hello i am reshma ");
+		Assert.assertTrue(true);
+	}
+}
